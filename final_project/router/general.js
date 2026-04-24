55705e3acb7331452fdbcd16a115e6c9ab7b77e1
Ranim <ranimhamrouni@gmail.com>
2026-04-24 13:31:36 +0100

    task 12-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,31 +25,49 @@ public_users.post("/register" ,(req,res) => {
 })
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+public_users.get('/', async function (req, res) {
+  try {
+    const getAllBooks = new Promise((resolve,reject) => {
+      if(books) resolve(books);
+      else reject("Unable to find books");
+    })
+    const data = await getAllBooks;
+    res.send(JSON.stringify(data,null,4));
+  } catch(err) {
+    res.status(500).json({message:"Error fetching books"});
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  if(books[isbn]){
-    res.send(JSON.stringify(books[isbn],null,4));
-  } else {
-    res.send("Book not found");
- }
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const getBook = new Promise((resolve,reject) => {
+      if(books[isbn]) resolve(books[isbn]);
+      else reject("Unable to find book");
+    })
+    const data = await getBook;
+    res.send(JSON.stringify(data,null,4));
+  } catch(err) {
+    res.status(500).json({message:"Error fetching book"});
+  }
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  let bookList =  Object.values(books);
-  bookList = bookList.filter(book => book.author === author);
-  if(bookList.length > 0){
-    res.send(JSON.stringify(bookList,null,4));
-  } else {
-    res.send("No books found for the given author");
+public_users.get('/author/:author', async function (req,res) {
+  try {
+    const author = req.params.author;
+    const getBook = new Promise((resolve,reject) => {
+      bookList = Object.values(books).filter((book) => book.author === author);
+      if(bookList.length>0) resolve(bookList);
+      else reject("Unable to find books");
+    }) 
+    const data = await getBook;
+    res.send(JSON.stringify(data,null,4));
+  } catch(err) {
+      res.status(500).json({message:"Error fetching book"});
   }
-});
+})
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
